99749a8b2c2a8e16dcde1990c07c90ce2fbb80a9
ridwaanhall <91854659+ridwaanhall@users.noreply.github.com>
2023-11-25 09:41:03 +0000

    controller

---
+++ server/views.py
@@ -0,0 +1,40 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from .serializers import UserSerializer
+from rest_framework import status
+from rest_framework.authtoken.models import Token
+from django.contrib.auth.models import User
+
+from django.shortcuts import get_object_or_404
+
+@api_view(['POST'])
+def signup(request):
+    serializer = UserSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        user = User.objects.get(username=serializer.data['username'])
+        user.set_password(request.data['password'])
+        token = Token.objects.create(user=user)
+        user.save()
+        return Response({"token": token.key, "user": serializer.data}, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['POST'])
+def login(request):
+    user = get_object_or_404(User, username=request.data['username'])
+    if not user.check_password(request.data['password']):
+        return Response({"error": "Wrong credentials"}, status=status.HTTP_400_BAD_REQUEST)
+    token, created = Token.objects.get_or_create(user=user)
+    serializer = UserSerializer(user)
+    return Response({"token": token.key, "user": serializer.data})
+
+from rest_framework.decorators import authentication_classes, permission_classes
+from rest_framework.authentication import SessionAuthentication, TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+
+@api_view(['GET'])
+@authentication_classes([SessionAuthentication, TokenAuthentication])
+@permission_classes([IsAuthenticated])
+def test_token(request):
+    return Response('passed for {}'.format(request.user.username))
